2a7b054d4b269cb8cf5a5d73895f97fb24ab4239
chic-caller-7520 <arbrown110@gmail.com>
2020-03-27 16:58:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,10 +12,10 @@ def add_element_to_end_of_array(array,element)
    array << element
 end  
 
-def add_element_to_start_of_array(element)
+def add_element_to_start_of_array(array)
   array = ["I", "am", "really", "learning"]
    
-  array.unshift(element)
+  array.unshift(array)
  
   
 end
